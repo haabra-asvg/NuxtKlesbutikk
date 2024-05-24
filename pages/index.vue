f3a23ebd9e53264f1dd37670f1e1d2a8939db79e
haabra-asvg <e-haabra2@iskule.no>
2024-05-24 16:44:06 +0200

    hjelp meg jeg finner ikke livsgleden

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,7 @@
 </script>
 
 <template>
-  <p>test</p>
-
-  <form action="/api/posts/create" method="POST">
-    <input type="text" name="test">
-    <button type="submit">Submit</button>
-  </form>
+  
 </template>
 
 <style scoped lang="scss">
